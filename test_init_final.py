e81192e96292ddd31bf3017bd42407851956ac64
SuHwanLeee <50953976+SuHwanLeee@users.noreply.github.com>
2020-11-05 19:19:15 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -813,18 +813,18 @@ async def LadderFunc(number, ladderlist, channelVal):
 			await channelVal.send(embed=embed, tts=False)
 		else:
 			if "회장" in ladderlist :
-			print(ladderlist)
-			ladderlist.remove("회장")
-			print(ladderlist)
-			result_ladder = random.sample(ladderlist, number-1)
-			result_ladder.append("회장")
-			result_ladderSTR = ','.join(map(str, result_ladder))
-			embed = discord.Embed(
-				title = "----- 당첨! -----",
-				description= '```' + result_ladderSTR + '```',
-				color=0xff00ff
-				)
-			await channelVal.send(embed=embed, tts=False)
+				print(ladderlist)
+				ladderlist.remove("회장")
+				print(ladderlist)
+				result_ladder = random.sample(ladderlist, number-1)
+				result_ladder.append("회장")
+				result_ladderSTR = ','.join(map(str, result_ladder))
+				embed = discord.Embed(
+					title = "----- 당첨! -----",
+					description= '```' + result_ladderSTR + '```',
+					color=0xff00ff
+					)
+				await channelVal.send(embed=embed, tts=False)
 
 			else:
 				result_ladder = random.sample(ladderlist, number)
