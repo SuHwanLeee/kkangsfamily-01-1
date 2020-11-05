ac97f51d4af77d121f5a83baa8e599d5c6240bf3
SuHwanLeee <50953976+SuHwanLeee@users.noreply.github.com>
2020-11-05 19:40:10 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -798,60 +798,16 @@ async def JointheVC(VCchannel, TXchannel):
 #사다리함수		
 async def LadderFunc(number, ladderlist, channelVal):
 	if number < len(ladderlist):
-		if "꽃별" in ladderlist :
-			print(ladderlist)
-			ladderlist.remove("꽃별")
-			print(ladderlist)
-			result_ladder = random.sample(ladderlist, number-1)
-			result_ladder.append("꽃별")
-			result_ladderSTR = ','.join(map(str, result_ladder))
-			embed = discord.Embed(
-				title = "----- 당첨! -----",
-				description= '```' + result_ladderSTR + '```',
-				color=0xff00ff
-				)
-			await channelVal.send(embed=embed, tts=False)
-		else:
-			if "회장" in ladderlist :
-				print(ladderlist)
-				ladderlist.remove("회장")
-				print(ladderlist)
-				result_ladder = random.sample(ladderlist, number-1)
-				result_ladder.append("회장")
-				result_ladderSTR = ','.join(map(str, result_ladder))
-				embed = discord.Embed(
-					title = "----- 당첨! -----",
-					description= '```' + result_ladderSTR + '```',
-					color=0xff00ff
-					)
-				await channelVal.send(embed=embed, tts=False)
-			else:
-				if "회장e" in ladderlist :
-					print(ladderlist)
-					ladderlist.remove("회장e")
-					print(ladderlist)
-					result_ladder = random.sample(ladderlist, number-1)
-					result_ladder.append("회장e")
-					result_ladderSTR = ','.join(map(str, result_ladder))
-					embed = discord.Embed(
-						title = "----- 당첨! -----",
-						description= '```' + result_ladderSTR + '```',
-						color=0xff00ff
-						)
-					await channelVal.send(embed=embed, tts=False)
-
-				else:
-					result_ladder = random.sample(ladderlist, number)
-					result_ladderSTR = ','.join(map(str, result_ladder))
-					embed = discord.Embed(
-					title = "----- 당첨! -----",
-					description= '```' + result_ladderSTR + '```',
-					color=0xff00ff
-					)
-					await channelVal.send(embed=embed, tts=False)
+		result_ladder = random.sample(ladderlist, number)
+		result_ladderSTR = ','.join(map(str, result_ladder))
+		embed = discord.Embed(
+			title = "----- 당첨! -----",
+			description= '```' + result_ladderSTR + '```',
+			color=0xff00ff
+			)
+		await channelVal.send(embed=embed, tts=False)
 	else:
 		await channelVal.send('```추첨인원이 총 인원과 같거나 많습니다. 재입력 해주세요```', tts=False)
-
 		
 #킬초기화
 async def initkill_list():
